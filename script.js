afa29e1b5cd70f0a4abad86f7052ed4fa320bf81
Jamie Jones <30621704+DrejarN@users.noreply.github.com>
2019-09-19 14:29:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //Vår JS kod    
+<<<<<<< HEAD
 
 document.getElementById('button').addEventListener('click', function(){
     document.querySelector('.darker').style.display = 'flex';
@@ -8,3 +9,22 @@ document.querySelector('.close').addEventListener('click', function(){
     document.querySelector('.darker').style.display = 'none';
 
 });
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 3000); // Change image every 2 seconds
+  }
+=======
+>>>>>>> cabef419ad3326ac60a98d96c0212f2d7039f7fc
