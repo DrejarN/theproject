8125d40a5bb40525d19732c10ef96b8948274619
Sofia <sofia.svensson97@hotmail.com>
2019-09-19 13:30:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //Vår JS kod    
+
+document.getElementById('button1').addEventListener('click', function(){
+    document.querySelector('.darker').style.display = 'flex';
+});
+
+document.querySelector('.close').addEventListener('click', function(){
+    document.querySelector('.darker').style.display = 'none';
+
+});
