db36e15a6011274f32f6b9d671bc44b483c37a0c
Jamie Jones <30621704+DrejarN@users.noreply.github.com>
2019-09-24 14:54:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
 //Vår JS kod   
 
-/*
+
 $(".stripes").click(function() {
     $("ul").slideToggle();
     $("ul li").css("display", "block");
@@ -13,11 +12,8 @@ $(window).resize(function(){
     }
 });
 
-=======
-//Vår JS kod
->>>>>>> 833b1a873c3685f8d61dd317a1729843905fe793
 
-document.getElementByClassName('.show').addEventListener('click', function(){
+document.getElementById('button').addEventListener('click', function(){
     document.querySelector('.darker').style.display = 'flex';
 });
 
@@ -25,26 +21,7 @@ document.querySelector('.close').addEventListener('click', function(){
     document.querySelector('.darker').style.display = 'none';
 
 });
-<<<<<<< HEAD
-*/
-=======
 
-
-$(document).ready(function(){
-    $(".stripes").click(function() {
-        $("ul").slideToggle();
-        $("ul li").css("display", "block");
-      });
-    
-    $(window).resize(function(){
-        if($(window).width() < 1040){
-            $("ul").removeAttr('style');
-        }
-    });
-    
-});
-
->>>>>>> 833b1a873c3685f8d61dd317a1729843905fe793
 var myTimeOut;
 
 function showSlides() {
@@ -64,15 +41,3 @@ function showSlides() {
     dots[slideIndex-1].className += " active";
     myTimeOut = setTimeout(showSlides, 4000); // Byter bild var 4:e sek
   }
-
-  /*
-  window.onload = function() {
-    if (window.jQuery) {  
-        // jQuery is loaded  
-        alert("Yeah!");
-    } else {
-        // jQuery is not loaded
-        alert("Doesn't Work");
-    }
-}
-*/
