4bb6941950287422e10f5fed6b8414f7e1af182e
Jamie Jones <30621704+DrejarN@users.noreply.github.com>
2019-09-26 21:34:14 +0200

    Update validation.js

--- validation.js
+++ validation.js
@@ -1,3 +1,58 @@
+// VALIDERING
+
+//Namn
+$(document).ready(function() {
+    var $regexname=/^([a-zA-Z]{3,30})$/;
+    $('#fname').keyup(function() {
+      if (!$(this).val().match($regexname)) {
+          console.log("Ingen match");
+           $('.emsg1').removeClass('hidden1');
+           $('.emsg1').show();
+       }
+     else {
+          $('.emsg1').addClass('hidden1');
+         }
+  });
+});
+
+
+//Telefon
+$(document).ready(function() {
+    var $regexnumber= /^[0-9]*$/g;
+    $('#phoneNr').keyup(function() {
+      if (!$(this).val().match($regexnumber)) {
+           $('.emsg2').removeClass('hidden2');
+           $('.emsg2').show();
+       }
+     else {
+          $('.emsg2').addClass('hidden2');
+         }
+  });
+});
+
+
+
+//Email
+$(document).ready(function() {
+    var $regexemail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    $('#email').keyup(function() {
+      if (!$(this).val().match($regexemail)) {
+          console.log("de matchar ej");
+           $('.emsg3').removeClass('hidden3');
+           $('.emsg3').show();
+       }
+     else {
+          $('.emsg3').addClass('hidden3');
+         }
+  });
+});
+
+
+//Meddelande, text-ruta
+
+
+
+/*
 function validera() {
     var förnamn = document.contactForm.firstname.value;
     var telefonnummer = document.contactForm.phonenumber.value;
@@ -22,3 +77,4 @@ function validera() {
 
         return isValid;
     }
+    */
