c35c246eee936159de1d9a93d07fe382df75ec32
Jamie Jones <30621704+DrejarN@users.noreply.github.com>
2019-10-01 08:53:52 +0200

    Update validation.js

--- validation.js
+++ validation.js
@@ -1,68 +1,68 @@
 // VALIDERING
 
 //Validerar namn. Får innehålla små&stora bokstäver, mellanrum och vara mellan 3-30 karaktärer.
-$(document).ready(function() {
-    var $regexname = /^([a-zA-Z\s]{3,30})$/;  
-    $('#fname').keyup(function() {
-      if (!$(this).val().match($regexname)) {
-          console.log("Ingen match");
-           $('.emsg1').removeClass('hidden1');
-           $('.emsg1').show();
-       }
-     else {
-          $('.emsg1').addClass('hidden1');
-         }
+$(document).ready(function () {
+  var $regexname = /^([a-zA-Z\s]{3,30})$/;
+  $('#fname').keyup(function () {
+    if (!$(this).val().match($regexname)) {
+      console.log("Ingen match");
+      $('.emsg1').removeClass('hidden1');
+      $('.emsg1').show();
+    }
+    else {
+      $('.emsg1').addClass('hidden1');
+    }
   });
 });
 
 
 //Validerar Telefonnummer. Får innehålla siffror enbart.
-$(document).ready(function() {
-    var $regexnumber= /^[0-9]*$/g;
-    $('#phoneNr').keyup(function() {
-      if (!$(this).val().match($regexnumber)) {
-           $('.emsg2').removeClass('hidden2');
-           $('.emsg2').show();
-       }
-     else {
-          $('.emsg2').addClass('hidden2');
-         }
+$(document).ready(function () {
+  var $regexnumber = /^[0-9]*$/g;
+  $('#phoneNr').keyup(function () {
+    if (!$(this).val().match($regexnumber)) {
+      $('.emsg2').removeClass('hidden2');
+      $('.emsg2').show();
+    }
+    else {
+      $('.emsg2').addClass('hidden2');
+    }
   });
 });
 
 
 //Validerar email enligt hur en email ska se ut: xxx@xxx.xxx
-$(document).ready(function() {
-    var $regexemail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    $('#email').keyup(function() {
-      if (!$(this).val().match($regexemail)) {
-           $('.emsg3').removeClass('hidden3');
-           $('.emsg3').show();
-       }
-     else {
-          $('.emsg3').addClass('hidden3');
-         }
+$(document).ready(function () {
+  var $regexemail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  $('#email').keyup(function () {
+    if (!$(this).val().match($regexemail)) {
+      $('.emsg3').removeClass('hidden3');
+      $('.emsg3').show();
+    }
+    else {
+      $('.emsg3').addClass('hidden3');
+    }
   });
 });
 
 
 //Validering Meddelande. Säger ifrån om rutan är rom.
-$(document).ready(function() {
+$(document).ready(function () {
   var $regexEmptyForm = /^$/;
-  $('#subject').keyup(function() {
+  $('#subject').keyup(function () {
     if ($(this).val().match($regexEmptyForm)) {
-         $('.emsg4').removeClass('hidden4');
-         $('.emsg4').show();
-     }
-   else {
-        $('.emsg4').addClass('hidden4');
-       }
+      $('.emsg4').removeClass('hidden4');
+      $('.emsg4').show();
+    }
+    else {
+      $('.emsg4').addClass('hidden4');
+    }
   });
 });
 
 //Validering på Submit - Ifall rutorna för namn eller text är tomma (eller om det finns error från realtidsvalidering uppe), så får man upp error.
-$(function() {
-  $('#sub').click(function() {
+$(function () {
+  $('#sub').click(function () {
     var $regexname = /^([a-zA-Z\s]{3,30})$/;
     var $regexemail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     var $regexEmptyForm = /^$/;
